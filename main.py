009da68f6377b9d2b3c34faedd8fc0de00a926a3
CompraCodigos <142620160+CompraCodigos@users.noreply.github.com>
2024-09-25 23:15:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+jugadores = ["Cavani", "Merentiel", "Zeballos"]
+print(jugadores[0])
+
+posiciones = ("Arquero", "Defensor", "Mediocampista", "Delantero")
+print(posiciones[3])  # Imprime "Mediocampista"
+
+estadisticas = {"goles": 37, "asistencias": 3, "partidos": 10}
+print(estadisticas["goles"])  # Imprime el número de goles
